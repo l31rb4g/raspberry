defb20c1a196c6f4629999ef704ff1648e24541a
Gabriel Paladino <gabriel.paladino@pearson.com>
2015-12-03 12:03:37 -0200

    pump

--- pump.py
+++ pump.py
@@ -15,41 +15,52 @@ class Pump:
         '4': 13
     }
 
+    channel = 3
+
     time = {
         'full': 80,
         'empty': 200
     }
 
+    silence = False
+
     def __init__(self):
-        cn = 3
-        c = self.channels[str(cn)]
+        c = self.channels[str(self.channel)]
+
+        if len(sys.argv) > 1 and sys.argv[1] == '--silence':
+            self.silence = True
 
         if c:
             GPIO.setmode(GPIO.BOARD)
             try:
                 GPIO.setup(c, GPIO.OUT)
                 while True:
-                    print('\n>>> Ligando bomba por ' + self.duration(self.time['full']))
+                    if not self.silence:
+                        print('\n>>> Ligando bomba por ' + self.duration(self.time['full']))
                     GPIO.output(c, GPIO.LOW)
                     self.wait(self.time['full'], 'Enchendo')
 
-                    print('\n>>> Desligando bomba por ' + self.duration(self.time['empty']))
+                    if not self.silence:
+                        print('\n>>> Desligando bomba por ' + self.duration(self.time['empty']))
                     GPIO.output(c, GPIO.HIGH)
                     self.wait(self.time['empty'], 'Esvaziando')
 
             except KeyboardInterrupt:
-                print('\n\n>>> Desligando tudo\n')
+                if not self.silence:
+                    print('\n\n>>> Desligando tudo\n')
                 GPIO.cleanup()
 
     def wait(self, n, msg):
         while True:
-            sys.stdout.write('\r' + msg + ' ... ' + self.duration(n) + ' ')
-            sys.stdout.flush()
+            if not self.silence:
+                sys.stdout.write('\r' + msg + ' ... ' + self.duration(n) + ' ')
+                sys.stdout.flush()
             n -= 1
             sleep(1)
             if n == 0:
-                sys.stdout.write('\r' + (' ' * 20))
-                sys.stdout.flush()
+                if not self.silence:
+                    sys.stdout.write('\r' + (' ' * 20))
+                    sys.stdout.flush()
                 break
 
     def duration(self, n):
