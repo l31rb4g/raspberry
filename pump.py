1ad000a3aeabee6285e0d1d30e33dabc07a59718
Gabriel Paladino <gabriel.paladino@pearson.com>
2015-12-03 10:11:39 -0200

    pump

--- pump.py
+++ pump.py
@@ -1,39 +1,49 @@
 #!/usr/bin/python
+#encoding: utf-8
 import sys
 import RPi.GPIO as GPIO
 from time import sleep
 
-channels = {
-    '1': 18,
-    '2': 16,
-    '3': 15,
-    '4': 13
-}
+class Pump:
 
-GPIO.setmode(GPIO.BOARD)
+    channels = {
+        '1': 18,
+        '2': 16,
+        '3': 15,
+        '4': 13
+    }
 
-c = False
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
 
-if len(sys.argv) > 1:
-    n = sys.argv[1]
-    if int(n) >= 1 and int(n) <= 4:
-        c = channels[sys.argv[1]]
-    else:
-        print('Canal invalido: ' + n)
-else:
-    print('Digite o numero do canal')
+        c = False
 
-if c:
-    try:
-        GPIO.setup(c, GPIO.OUT)
+        if len(sys.argv) > 1:
+            n = sys.argv[1]
+            if int(n) >= 1 and int(n) <= 4:
+                c = self.channels[sys.argv[1]]
+            else:
+                print('Canal invalido: ' + n)
+        else:
+            print('Digite o número do canal')
+
+        if c:
+            try:
+                GPIO.setup(c, GPIO.OUT)
+                while True:
+                    self.wait(80, 'Enchendo')
+                    GPIO.output(c, GPIO.LOW)
+                    self.wait(200, 'Esvaziando')
+                    GPIO.output(c, GPIO.HIGH)
+
+            except KeyboardInterrupt:
+                GPIO.cleanup()
+
+    def wait(self, n, msg):
         while True:
-            print "Enchendo"
-            GPIO.output(c, GPIO.LOW)
-            sleep(80)
-            print("Ezvaziando")
-            GPIO.output(c, GPIO.HIGH)
-            sleep(200)
-    
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-
+            sys.stdout.write('\r' + msg + ' ... ' + str(n) + ' ')
+            sys.stdout.flush()
+            n -= 1
+            sleep(1)
+            if n == 0:
+                break
